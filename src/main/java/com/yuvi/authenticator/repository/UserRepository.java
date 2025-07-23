0a557660e25619041a8126bc90485274d48033fb
yuvaraj <yuvaraj.codes@gmail.com>
2025-07-23 19:30:33 +0530

    add UserRepo layer

---
+++ src/main/java/com/yuvi/authenticator/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.yuvi.authenticator.repository;
+
+import com.yuvi.authenticator.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByVerificationCode(String verificationCode);
+}
