03f52bd08da97f003fe5ae812555f21a085aeefb
yuvaraj <yuvaraj.codes@gmail.com>
2025-07-24 21:59:31 +0530

    add app config

---
+++ src/main/java/com/yuvi/authenticator/config/ApplicationConfig.java
@@ -0,0 +1,49 @@
+package com.yuvi.authenticator.config;
+
+import com.yuvi.authenticator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ApplicationConfig(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+}
---
+++ src/main/java/com/yuvi/authenticator/config/JwtAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.yuvi.authenticator.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationFilter {
+}
