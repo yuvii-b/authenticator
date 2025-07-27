8c2384af274216e78eb4247e6ba21f8d0446e839
yuvaraj <yuvaraj.codes@gmail.com>
2025-07-27 13:33:35 +0530

    add post-autorize funcs

---
+++ src/main/java/com/yuvi/authenticator/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.yuvi.authenticator.controller;
+
+import com.yuvi.authenticator.model.User;
+import com.yuvi.authenticator.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers(){
+        return ResponseEntity.ok(userService.allUsers());
+    }
+}
---
+++ src/main/java/com/yuvi/authenticator/service/UserService.java
@@ -0,0 +1,23 @@
+package com.yuvi.authenticator.service;
+
+import com.yuvi.authenticator.model.User;
+import com.yuvi.authenticator.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository, EmailService emailService){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
